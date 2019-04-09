8b9011ff22dc5ce9ff23e74bfa8bf822283ea7f7
Alan <pchiu4@u.rochester.edu>
2019-04-09 10:47:38 -0400

    initialize header

---
+++ ullmann.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <utility>
+#include <thread>
+#include <atomic>
+#include <chrono>
+#include <cstring>
+#include <string>
+#include <mutex>
+
+
---
+++ ullmann.hpp
@@ -0,0 +1,46 @@
+#include <cstdlib>
+#include <ctime>
+#include <climits>
+#include <iostream>
+#include <vector>
+#include <set>
+#include <utility>
+#include <thread>
+#include <atomic>
+#include <chrono>
+#include <cstring>
+#include <string>
+#include <mutex>
+
+using namespace std;
+
+
+class Vertex;
+
+
+class Vertex{
+public:
+	int id;
+	int degree;
+	vector<Edge> incidentSet;
+
+	Vertex();
+	Vertex(int id);
+
+
+	bool operator<(const Vertex& v2) const;
+	bool operator==(const Vertex& v2) const;
+	bool operator>(const Vertex& v2) const;
+};
+
+
+class Edge{
+public:
+	Vertex* v1; 
+	Vertex* v2;
+
+	Edge();
+	Edge(Vertex* vertex1, Vertex* vertex2);
+	Vertex* other(Vertex v) const;
+	
+};
