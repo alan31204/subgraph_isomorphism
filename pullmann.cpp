bd1c8c6e4dd9f205f2869f0c9eda05b78aec7197
Alan <pchiu4@u.rochester.edu>
2019-04-23 11:59:47 -0400

    first attempt for cilk spawn ullmann

--- pullmann.cpp
+++ pullmann.cpp
@@ -129,13 +129,122 @@ int vnumB;
 
 // Do the call of Cilk_spawn to split the work for different threads
 bool ullmann_spawn(Graph& gA, Graph& gB, bool* carray, vector<pair<int, int>> work_split){
+	bool rcarray[vnumA][vnumB];
+	for(pair<int, int> task : work_split){
+		memcpy(rcarray, carray, vnumA * vnumB * sizeof(bool));
+		for(int x = 0;x < vnumA;x++)			// remove c from candidates(x) for all x in gA
+			rcarray[x][task.second] = false;
+		for(int y = 0;y < vnumB;y++)			// remove all y from candidates(i)
+			rcarray[task.first][y] = false;
+		rcarray[task.first][task.second] = true;	// re-add c to candidates(i) as sole member
 
+		// recursively call ullman with rcarray
+		if(ullmann_descent(gA,gB,&rcarray[0][0])) return true;
+	}
+	return false; // if the recursive result didn't give true
 }
 
 
 bool ullmann_descent(Graph& gA, Graph& gB, bool* carray){
+	vector<int> cneighbors;
+	Vertex v;
+	bool ret, disjoint, solved, rcarray[vnumA][vnumB];
+	int numCandidates[vnumA], numCandidatesFor[vnumB];
+
+	vector<pair<int, int>> work, work_split;
+
+	// secondary pruning
+	for(int i = 0;i < vnumA;i++){						// iterate over each vertex id i in gA
+		v = gA.findIndex(i);							// find vertex v with id i
+		for(int c = 0;c < vnumB;c++){					// iterate over candidates for vertex
+			if(!carray[RIDX(i, c, vnumB)]) continue;	//   by skipping over noncandidates
+			cneighbors = gB.findIndex(c).neighbors();	// find neighbors of selected candidate
+			for(int n : v.neighbors()){					// iterate over neighbors of v and 
+				disjoint = true;						//   determine if cneighbors and
+														//   v.neighbors() are disjoint
+				for(int cn : cneighbors){
+					if(carray[RIDX(n, cn, vnumB)]){
+						disjoint = false;
+						break;
+					}
+				}
+
+				// if cneighbors and v.neighbors() are disjointthen c must be removed from v's
+				//   candidate set, and then we can check v's next candidate, else, check for
+				//   next neighbor
+				if(disjoint){
+					carray[RIDX(i, c, vnumB)] = false;
+					break;
+				}
+			}
+		}
+	}
+
+	// add up the rows of carray to get the number of candidates per vertex in gA
+	for(int i = 0;i < vnumA;i++) numCandidates[i] = 0;
+	for(int i = 0;i < vnumA;i++){
+		for(int c  = 0;c < vnumB;c++)
+			numCandidates[i] += (int) carray[RIDX(i, c, vnumB)];
+	}
+
+	// add up the cols of carray to get the number of times each vertex in gB is a candidate
+	//   for a vertex in gA
+	for(int c = 0;c < vnumB;c++) numCandidatesFor[c] = 0;
+	for(int c = 0;c < vnumB;c++){
+		for(int i = 0;i < vnumA;i++)
+			numCandidatesFor[c] += (int) carray[RIDX(i, c, vnumB)];
+	}
+
+	// assume carray contains a solution, then check if a row of carray is empty (return false),
+	//   if a row of carray has more than one candidate left (solved = false), or if a col of
+	//   carray has more than one true value (solved = false)
+	solved = true;
+	for(int i = 0;i < vnumA;i++){
+		if(numCandidates[i] == 0) return false; 		// empt row
+		else if(numCandidates[i] > 1) solved = false; 	// row has more than one candidate
+	}
+	for(int c = 0;c <  vnumB;c++)
+		if(numCandidatesFor[c] > 1) solved = false; 	// col has more than one true value
 	
-	return false;
+	// return true if carray contains a solution, otherwise continue recursion
+	if(solved) return true;
+
+	// pick an i in gA, c in candidates(i) where |candidates(i)| > 1 and assign i to c in
+	//   rcarray (otherwise a copy of carray) and then pass rcarray to new recursive call
+
+	work = vector<pair<int,int>>();
+	for(int i = 0;i < vnumA;i++){
+		if(numCandidates[i] == 1) continue;	// skip vertex if already assigned
+		for(int c = 0;c < vnumB;c++){
+			if(!carray[RIDX(i, c, vnumB)]) continue;
+			work.push_back(make_pair(i,c));
+		}
+	}
+
+	work_split = vector<pair<int,int>>();
+	ret = false;
+	for(int n = 0; n < work.size();n++){
+		if(n < work.size()/2){
+			work_split.push_back(work[n]);
+			continue;
+		}
+		if(n == work.size()/2)
+			ret = cilk_spawn ullmann_spawn(gA, gB, carray, work_split);
+		if(ret) return true;
+
+		memcpy(rcarray, carray, vnumA * vnumB * sizeof(bool));
+		for(int x = 0;x < vnumA;x++)	// remove c from candidates(x) for all x in gA
+			rcarray[x][work[n].second] = false;
+		for(int y = 0;y < vnumB;y++)	// remove all y from candidates(i)
+			rcarray[work[n].first][y] = false;
+		rcarray[work[n].first][work[n].second] = true;			// re-add c to candidates(i) as sole member
+
+		// recursively call ullman with rcarray
+		if(ullmann_descent(gA,gB,&rcarray[0][0])) return true;
+	}
+	cilk_sync
+	// if no recursive call finds an isomorphim, return false
+	return ret;
 }
 
 // returns true if gA is a subgraph of gB and false otherwise
